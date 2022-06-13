c2efc1b08de7164018bb48a7481b9df035eb2b4a
Usuario <Usuario@CPD01-M13>
2022-06-13 15:20:00 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_Mercado/Lista_Mercado/Helper/SQLiteHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lista_Mercado.Model;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Lista_Mercado.Helper
+{
+    public class SQLiteHelper
+    {
+        readonly SQLiteAsyncConnection connection;
+
+        public void insert(Produto p)
+        {
+
+        }
+
+        public void update(Produto p)
+        {
+
+        }
+
+
+        //retorna preenchido
+        public Task<Produto> getById(int p)
+        {
+            return new Produto(); 
+        }
+
+        
+        public Task<List<Produto>> getAll()
+        {
+
+        }
+
+        public void delete(int id)
+        {
+
+        }
+    }
+}
